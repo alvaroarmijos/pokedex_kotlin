417b38f1fa52d9fef50526ad5f159d779dbeaa0e
Alvaro Armijos <alvaro.armijos@zemoga.com>
2023-04-23 19:21:12 -0500

    Create home UI

--- app/src/main/java/com/example/pokedexkotlin/feature/home/HomeActivity.kt
+++ app/src/main/java/com/example/pokedexkotlin/feature/home/HomeActivity.kt
@@ -3,10 +3,18 @@ package com.example.pokedexkotlin.feature.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pokedexkotlin.R
+import com.example.pokedexkotlin.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
